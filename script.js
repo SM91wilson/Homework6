2ae00fdddc3cbc2a52e30969b105aa1af836dc6a
tango <sm91wilson@gmail.com>
2020-03-17 19:08:06 -0400

    working on forecast

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+// var to get the current date
+var today = new Date();
+var currentDate = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+
 // event listener for the submit button
 $("button").on("click", function(e){
 
-  // var for the div called currentCity
+  // var for the div called currentCity and for one called forecast
   var currentCity = $(".current-city");
+  var forecast = $(".forecast");
   // getting the value of the input text
-  var city = $("#search").val();  
+  var city = $("#search").val(); 
   // my api key
   var APIKey = "ab35b7ff4acffaadd63092658010c663";
   // building the query url
@@ -13,23 +18,41 @@ $("button").on("click", function(e){
 
   //  emptying the current city div
   $(currentCity).empty();
+  $(forecast).empty();
   // stop the page from navigating away
   e.preventDefault();
   // the ajax call
     $.ajax({
-    url:queryURL,
-    method: "GET"
+      url:queryURL,
+      method: "GET"
     }).then(function(response){
     console.log(response);
 
+    // building the div with the information from the ajax call and the text input
+    $(currentCity).append($("<h2>").text(city + " (" + currentDate +")"));
+    $(currentCity).append($("<ul>"));
+    var weatherList = $("ul");
+    $(weatherList).append($("<li>").text("Temperature: " + ((((response.main.temp)-273.15)* 9/5 + 32).toFixed(1)) + "°F"));
+    $(weatherList).append($("<li>").text("Humidity: " + (response.main.humidity) + "%"));
+    $(weatherList).append($("<li>").text("Wind Speed: " + (response.wind.speed) + "MPH"));
+    
   
-  // building the div with the information from the ajax call and the text input
-  $(currentCity).append($("<h2>").text(city));
-  $(currentCity).append($("<ul>"));
-  var weatherList = $("ul");
-  $(weatherList).append($("<li>").text("Temperature: " + ((((response.main.temp)-273.15)* 9/5 + 32).toFixed(1)) + "°F"));
-  $(weatherList).append($("<li>").text("Humidity: " + (response.main.humidity) + "%"));
-  $(weatherList).append($("<li>").text("Wind Speed: " + (response.wind.speed) + "MPH"));
+      $.ajax({
+        url:"https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey,
+        method: "GET"
+      }).then(function(forecastDays){
+        console.log(forecastDays);
+        $(forecast).append($("<h5>").text("5 Day Forecast"));
+        
+
+      });
+      
+      
+
+    
+
+  
+  
 
   // array to store all searched for cities
   var cities = [];
@@ -42,5 +65,5 @@ $("button").on("click", function(e){
   // retrieve data from local storage
   // generate buttons from data the recalls the information from the response
   // ajax call for 5 day forecast
-})
+});
 });
