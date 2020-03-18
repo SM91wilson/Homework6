fbd6391503b39df9b5d5ee28673d606a1e91841f
tango <sm91wilson@gmail.com>
2020-03-17 20:23:10 -0400

    fixed forecast

--- script.js
+++ script.js
@@ -18,7 +18,12 @@ $("button").on("click", function(e){
 
   //  emptying the current city div
   $(currentCity).empty();
-  $(forecast).empty();
+  $("h4").remove();
+  $(".day2weather").empty();
+  $(".day3weather").empty();
+  $(".day4weather").empty();
+  $(".day5weather").empty();
+  $(".day6weather").empty();
   // stop the page from navigating away
   e.preventDefault();
   // the ajax call
@@ -30,8 +35,8 @@ $("button").on("click", function(e){
 
     // building the div with the information from the ajax call and the text input
     $(currentCity).append($("<h2>").text(city + " (" + currentDate +")"));
-    $(currentCity).append($("<ul>"));
-    var weatherList = $("ul");
+    $(currentCity).append($("<ul>").addClass("cwlist"));
+    var weatherList = $(".cwlist");
     $(weatherList).append($("<li>").text("Temperature: " + ((((response.main.temp)-273.15)* 9/5 + 32).toFixed(1)) + "°F"));
     $(weatherList).append($("<li>").text("Humidity: " + (response.main.humidity) + "%"));
     $(weatherList).append($("<li>").text("Wind Speed: " + (response.wind.speed) + "MPH"));
@@ -42,9 +47,33 @@ $("button").on("click", function(e){
         method: "GET"
       }).then(function(forecastDays){
         console.log(forecastDays);
-        $(forecast).append($("<h5>").text("5 Day Forecast"));
+        $(forecast).prepend($("<h4>").text("5 Day Forecast"));
         
+        var day2 = $(".day2");
+        var day3 = $(".day3");
+        var day4 = $(".day4");
+        var day5 = $(".day5");
+        var day6 = $(".day6");
 
+        $(".day2weather").append($("<li>").text(forecastDays.list[3].dt_txt));
+        $(".day2weather").append($("<li>").text("temp: " + (((((forecastDays.list[3].main.temp)-273.15)* 9/5 + 32).toFixed(1)) + "°F")));
+        $(".day2weather").append($("<li>").text("humidity: " + forecastDays.list[3].main.humidity + "%"));
+
+        $(".day3weather").append($("<li>").text(forecastDays.list[11].dt_txt));
+        $(".day3weather").append($("<li>").text("temp: " + (((((forecastDays.list[11].main.temp)-273.15)* 9/5 + 32).toFixed(1)) + "°F")));
+        $(".day3weather").append($("<li>").text("humidity: " + forecastDays.list[11].main.humidity + "%"));
+
+        $(".day4weather").append($("<li>").text(forecastDays.list[19].dt_txt));
+        $(".day4weather").append($("<li>").text("temp: " + (((((forecastDays.list[19].main.temp)-273.15)* 9/5 + 32).toFixed(1)) + "°F")));
+        $(".day4weather").append($("<li>").text("humidity: " + forecastDays.list[19].main.humidity + "%"));
+
+        $(".day5weather").append($("<li>").text(forecastDays.list[27].dt_txt));
+        $(".day5weather").append($("<li>").text("temp: " + (((((forecastDays.list[27].main.temp)-273.15)* 9/5 + 32).toFixed(1)) + "°F")));
+        $(".day5weather").append($("<li>").text("humidity: " + forecastDays.list[27].main.humidity + "%"));
+
+        $(".day6weather").append($("<li>").text(forecastDays.list[35].dt_txt));
+        $(".day6weather").append($("<li>").text("temp: " + (((((forecastDays.list[35].main.temp)-273.15)* 9/5 + 32).toFixed(1)) + "°F")));
+        $(".day6weather").append($("<li>").text("humidity: " + forecastDays.list[35].main.humidity + "%"));
       });
       
       
